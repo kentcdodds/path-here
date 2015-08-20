46ea7e90a6fed888fd6b91df9058a0d4e9829c17
Kent C. Dodds <kent@doddsfamily.us>
2015-08-20 10:06:15 -0600

    feat(here): Add .dir function to specify the rootDirectory to resolve paths to
    Change the tests to go twice, once for the default case and once for the .dir case

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var path = require('path');
+module.exports = dir(process.cwd());
+module.exports.dir = dir;
 
-module.exports = here;
+function dir(rootDir) {
+  return here;
 
-// from babel.js
-// http://babeljs.io/repl/#?experimental=false&evaluate=false&loose=false&spec=false&playground=true&code=function%20here(...paths)%20%7B%0A%20%20paths.unshift(process.cwd())%3B%0A%20%20console.log(...paths)%3B%0A%7D
-function here() {
-  for (var _len = arguments.length, paths = Array(_len), _key = 0; _key < _len; _key++) {
-    paths[_key] = arguments[_key];
+  // from babel.js
+  // http://babeljs.io/repl/#?experimental=false&evaluate=false&loose=false&spec=false&playground=true&code=function%20here(...paths)%20%7B%0A%20%20paths.unshift(process.cwd())%3B%0A%20%20console.log(...paths)%3B%0A%7D
+  function here() {
+    for (var _len = arguments.length, paths = Array(_len), _key = 0; _key < _len; _key++) {
+      paths[_key] = arguments[_key];
+    }
+
+    paths.unshift(rootDir);
+    return path.join.apply(path, paths);
   }
-
-  paths.unshift(process.cwd());
-  return path.join.apply(path, paths);
 }
 
--- index.test.js
+++ index.test.js
@@ -3,43 +3,57 @@ var here = require('./index');
 
 describe('path-here', function() {
 
-  var srcDir = __dirname + '/src';
-  var slashedSrcDir = srcDir + '/';
-  var longDir = __dirname + '/path/to/dir';
-
-  it('should return __dirname when invoked with nothing', function() {
-    var nothing = here();
-    expect(nothing).to.equal(__dirname);
+  describe('default', function() {
+    testHereInstance(here, __dirname);
   });
 
-  it('should resolve paths without a slash', function() {
-    var slashlessPath = here('src');
-    expect(slashlessPath).to.equal(srcDir);
+  describe('dir', function() {
+    var startingDir = __dirname + '/starting/dir';
+    var startingHere = here.dir(startingDir);
+    testHereInstance(startingHere, startingDir);
   });
 
-  it('should resolve paths with a prefixed slash', function() {
-    var prefixedWithSlash = here('/src');
-    expect(prefixedWithSlash).to.equal(srcDir);
-  });
 
-  it('should resolve paths with a trailing slash', function() {
-    var trailingSlash = here('src/');
-    expect(trailingSlash).to.equal(slashedSrcDir);
-  });
+  function testHereInstance(hereInstance, rootDir) {
+    var srcDir = rootDir + '/src';
+    var slashedSrcDir = srcDir + '/';
+    var longDir = rootDir + '/path/to/dir';
 
-  it('should resolve paths with a prefix and trailing slash', function() {
-    var trailingAndPrefixedWithSlash = here('/src/');
-    expect(trailingAndPrefixedWithSlash).to.equal(slashedSrcDir);
-  });
+    it('should return rootDir when invoked with nothing', function() {
+      var nothing = hereInstance();
+      expect(nothing).to.equal(rootDir);
+    });
 
-  it('should resolve a longer list of slahshed directories', function() {
-    var slashedDirs = here('path/to/dir');
-    expect(slashedDirs).to.equal(longDir);
-  });
+    it('should resolve paths without a slash', function() {
+      var slashlessPath = hereInstance('src');
+      expect(slashlessPath).to.equal(srcDir);
+    });
 
-  it('should resolve directories given as additional arguments', function() {
-    var argsDirs = here('path', 'to', 'dir');
-    expect(argsDirs).to.equal(longDir);
-  });
+    it('should resolve paths with a prefixed slash', function() {
+      var prefixedWithSlash = hereInstance('/src');
+      expect(prefixedWithSlash).to.equal(srcDir);
+    });
+
+    it('should resolve paths with a trailing slash', function() {
+      var trailingSlash = hereInstance('src/');
+      expect(trailingSlash).to.equal(slashedSrcDir);
+    });
+
+    it('should resolve paths with a prefix and trailing slash', function() {
+      var trailingAndPrefixedWithSlash = hereInstance('/src/');
+      expect(trailingAndPrefixedWithSlash).to.equal(slashedSrcDir);
+    });
+
+    it('should resolve a longer list of slahshed directories', function() {
+      var slashedDirs = hereInstance('path/to/dir');
+      expect(slashedDirs).to.equal(longDir);
+    });
+
+    it('should resolve directories given as additional arguments', function() {
+      var argsDirs = hereInstance('path', 'to', 'dir');
+      expect(argsDirs).to.equal(longDir);
+    });
+
+  }
 });
 
